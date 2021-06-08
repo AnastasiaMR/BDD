bc863612d5600997e2e760528b11d43d420eb907
Anastasia Matryashina <matr-nastya@yandex.ru>
2021-06-08 19:16:44 +0300

    Modified code

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -10,23 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
 
-        @Test
-        void shouldTransferMoneyFromFirstCardToSecondCard() {
-            val loginPage = open("http://localhost:9999", LoginPageV1.class);
-            val authInfo = DataHelper.getAuthInfo();
-            val verificationPage = loginPage.validLogin(authInfo);
-            val verificationCode = DataHelper.getVerificationCode(authInfo);
-            val dashboardPage = verificationPage.validVerify(verificationCode);
-            val balanceFirstBillBeforeTransfer = dashboardPage.getFirstCardBalance();
-            val balanceSecondBillBeforeTransfer = dashboardPage.getSecondCardBalance();
-            val moneyTransfer = dashboardPage.secondBill();
-            int amount = 3000;
-            moneyTransfer.transferMoney(amount, DataHelper.CardNumber.getCardFirst());
-            val balanceFirstBillAfterTransfer = dashboardPage.getFirstCardBalance();
-            val balanceSecondBillAfterTransfer = dashboardPage.getSecondCardBalance();
-            assertEquals((balanceFirstBillBeforeTransfer - amount), balanceFirstBillAfterTransfer);
-            assertEquals((balanceSecondBillBeforeTransfer + amount), balanceSecondBillAfterTransfer);
-        }
+    @Test
+    void shouldTransferMoneyFromFirstCardToSecondCard() {
+        val loginPage = open("http://localhost:9999", LoginPageV1.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCode(authInfo);
+        val dashboardPage = verificationPage.validVerify(verificationCode);
+        val balanceFirstBillBeforeTransfer = dashboardPage.getFirstCardBalance();
+        val balanceSecondBillBeforeTransfer = dashboardPage.getSecondCardBalance();
+        val moneyTransfer = dashboardPage.secondBill();
+        int amount = 3000;
+        moneyTransfer.transferMoney(amount, DataHelper.CardNumber.getCardFirst());
+        val balanceFirstBillAfterTransfer = dashboardPage.getFirstCardBalance();
+        val balanceSecondBillAfterTransfer = dashboardPage.getSecondCardBalance();
+        assertEquals((balanceFirstBillBeforeTransfer - amount), balanceFirstBillAfterTransfer);
+        assertEquals((balanceSecondBillBeforeTransfer + amount), balanceSecondBillAfterTransfer);
+    }
 
     @Test
     void shouldTransferMoneyFromSecondCardToFirstCardNoValid() {
@@ -38,7 +38,7 @@ class MoneyTransferTest {
         val balanceFirstBillBeforeTransfer = dashboardPage.getFirstCardBalance();
         val balanceSecondBillBeforeTransfer = dashboardPage.getSecondCardBalance();
         val moneyTransfer = dashboardPage.firstBill();
-        int amount = -1500;
+        int amount = 35000;
         moneyTransfer.transferMoney(amount, DataHelper.CardNumber.getCardSecond());
         val balanceSecondBillAfterTransfer = dashboardPage.getSecondCardBalance();
         val balanceFirstBillAfterTransfer = dashboardPage.getFirstCardBalance();
